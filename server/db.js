54870359fecaf85e8df91c78da8c987437f79404
knnthdvlpr <kennacebuche@gmail.com>
2025-08-31 17:04:00 +0800

    added new config

--- server/db.js
+++ server/db.js
@@ -2,11 +2,25 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Render requires SSL
+  },
 });
 
 module.exports = pool;
+
+
+
+// require("dotenv").config();
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: process.env.DATABASE_USERNAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   host: process.env.DATABASE_HOST,
+//   port: process.env.DATABASE_PORT,
+//   database: process.env.DATABASE_NAME,
+// });
+
+// module.exports = pool;
