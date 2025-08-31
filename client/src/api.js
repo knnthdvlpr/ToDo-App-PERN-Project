6aa393f8fa127b85035c6e4c80ba9eeab5de3bb3
knnthdvlpr <kennacebuche@gmail.com>
2025-08-31 20:42:49 +0800

    change api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,2 +1,7 @@
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE = "https://todo-app-pern-azwk.onrender.com";
 export default API_BASE;
+
+
+
+// const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+// export default API_BASE;
